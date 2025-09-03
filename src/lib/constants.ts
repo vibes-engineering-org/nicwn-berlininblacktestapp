77f0ef9a55df5dbea45f09e3f2a1728163fd7879
vibesengineering <205912982+vibesengineering@users.noreply.github.com>
2025-09-03 12:11:44 +0000

    Code generation: 717f3512

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const PROJECT_TITLE = "Berlin in Black Test App";
-export const PROJECT_DESCRIPTION = "by @nicwn";
+export const PROJECT_DESCRIPTION = "NFT minting app featuring different collectible cards showcasing Berlin resources and services";
 export const PROJECT_CREATOR = "nicwn";
 export const PROJECT_AVATAR_URL =
   "https://wrpcd.net/cdn-cgi/imagedelivery/BXluQx4ige9GuW0Ia56BHw/37460dae-996e-481a-9c2c-0374da719400/anim=false,fit=contain,f=auto,w=576";
